bc0ad7643b2e964a6975a72b00742988e4ad24ea
Bontinck L <moutondemocrate@gmail.com>
2023-10-13 00:04:36 +0200

    Grav ?

---
+++ grav/cache/compiled/files/068c442f8d4be583c3e70b94aa9a0dc5.yaml.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/build/grav/user/config/security.yaml',
+    'modified' => 1689699306,
+    'size' => 21,
+    'data' => [
+        'salt' => '7sC5UMS6aNJAip'
+    ]
+];
